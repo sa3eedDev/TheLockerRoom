30f4bae8144d9f0284ab4e708edba0933cd6788d
Saeed Binsalman <saeedbinsalman@Saeeds-MacBook-Air.local>
2021-10-20 18:10:27 -0400

    added databse script for player

---
+++ database/looker_room.sql
@@ -0,0 +1,17 @@
+create database the_locker_room;
+
+use the_locker_room;
+
+create table player(
+    idPlayer int AUTO_INCREMENT not null,
+    firstName varchar(25),
+    lastName varchar(25),
+    userName varchar(25),
+    email varchar(50),
+    phoneNumber varchar(14),
+    profilePicture varchar(1024),
+    gender varchar(5),
+    passwd varchar(1024),
+    email unique key,
+    idPlayer primary key
+)
